3203f358afb0e725629657373d4586d1a4f82768
jun-sugawara <ex.24karats.0110@i.softbank.jp>
2022-12-08 21:09:59 +0900

    DB設定

---
+++ db/migrate/20221208114811_create_users.rb
@@ -0,0 +1,8 @@
+class CreateUsers < ActiveRecord::Migration[6.0]
+  def change
+    create_table :users, comment: 'ゲームのユーザー情報を管理するテーブル' do |t|
+      t.string :name, null: false, comment: 'ユーザーの名前'
+      t.timestamps null: false
+    end
+  end
+end
